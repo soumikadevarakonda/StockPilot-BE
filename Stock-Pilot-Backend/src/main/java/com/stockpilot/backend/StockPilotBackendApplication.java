b0fe062699c9c5dd1ab240611bf9262a13da2aa2
soumikadevarakonda <soumikadevarakonda@gmail.com>
2025-11-03 08:03:38 +0530

    Initial commit: Spring Boot setup for StockPilot backend

---
+++ Stock-Pilot-Backend/src/main/java/com/stockpilot/backend/StockPilotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stockpilot.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPilotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPilotBackendApplication.class, args);
+	}
+
+}
---
+++ Stock-Pilot-Backend/src/test/java/com/stockpilot/backend/StockPilotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockpilot.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPilotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
